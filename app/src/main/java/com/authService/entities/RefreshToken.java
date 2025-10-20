7669a7a1c383854c111876cc57b7ca63a6d1ed7d
sinster2003 <sindhurvshabaraya2318@gmail.com>
2025-10-20 20:32:37 +0530

    added refresh token (tokens) entity with one to one mapping

---
+++ app/src/main/java/com/authService/entities/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.authService.entities;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder // implements builder design pattern - contains an inner class to abstract complex object creation
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) // serialises to snake case instead of default camel case
+@Table(name = "tokens")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // delegates the id generation to the database
+    private Long id; // Generated automatically by DB using auto increment
+
+    private String token; // refresh token
+
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "user_id") // user_id is the foreign key
+    private UserInfo userInfo;
+}
